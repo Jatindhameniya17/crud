405ba3a51e944ca63316bcc8fc6702db60ba51e3
Jatindhameniya17 <jatindhameniya1706@gmail.com>
2025-05-02 10:06:46 +0530

    add mongo

--- new-connection/backend/database.js
+++ new-connection/backend/database.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 async function dbConn(){
-    const conn=await mongoose.connect('mongodb://localhost:27017/studentapp');
+    const conn=await mongoose.connect('mongodb+srv://jatindhameniya:root@cluster0.ewivo8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     if(conn){
         console.log('database connect successfully')
 
